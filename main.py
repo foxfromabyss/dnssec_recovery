99565fb8d23886f4e173050cf06355fd07eabb70
foxfromabyss <darkell@list.ru>
2020-09-02 09:51:54 +0200

    Introduce rudimentary avro

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from crack import grab_signature, grab_pubkey, crack
 from fastavro import writer, reader, parse_schema
+import json
 
-with open('data/CO_D4F19FC155EC8C00296607920B539F56.avro', 'rb') as fo:
+file_name = 'data/CO_D4F19FC155EC8C00296607920B539F56.avro'
+with open(file_name, 'rb') as fo:
     for record in reader(fo):
+        # should group records together and then reprocess them?
         print(record)
 
 #crack(5,6,7,8,9,0,11)
